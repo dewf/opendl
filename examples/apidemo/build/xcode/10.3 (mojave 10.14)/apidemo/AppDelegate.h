a1a9d698731c02a65158b876d04d49fd4fda2b36
Daniel X <daniel@Daniels-iMac.local>
2019-08-08 16:24:49 -0600

    mojave xcode build files

---
+++ examples/apidemo/build/xcode/10.3 (mojave 10.14)/apidemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  apidemo
+//
+//  Created by Daniel G on 11/15/18.
+//  Copyright (c) 2018 OpenDL Developers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ examples/apidemo/build/xcode/10.3 (mojave 10.14)/apidemo/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  apidemo
+//
+//  Created by Daniel G on 11/15/18.
+//  Copyright (c) 2018 OpenDL Developers. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
